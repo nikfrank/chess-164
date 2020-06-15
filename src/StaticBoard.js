3a1303c7cb198388790519c78e408b34469f696c
nik frank <nikbfrank@gmail.com>
2020-06-16 00:05:46 +0300

    static board sidenav styles

--- src/StaticBoard.js
+++ src/StaticBoard.js
@@ -4,7 +4,13 @@ import Piece from 'react-chess-pieces';
 
 function StaticBoard({ pieces, turn, flipped }){
   return (
-    <div className='Board' style={{ width: '30vw', height: '30vw' }}>
+    <div className='Board'
+         style={{
+           width: '30vw', height: '30vw',
+           maxWidth: 250, maxHeight: 250,
+           margin: '20px auto',
+           flexDirection: flipped ? 'column' : 'column-reverse'
+         }}>
       {Array(8).fill(0).map((_, rank)=> (
          <div className='rank' key={rank}>
            {pieces.slice(rank*8, rank*8+8).map((piece, file)=> (
